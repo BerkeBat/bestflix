764f6a3fa1b88bf64160d79980440230a29620ab
Berke <s3782067@student.rmit.edu.au>
2021-05-05 16:30:58 +1000

    moved helper methods to seperate DAO file. minor formatting of search and user page movie cards.

---
+++ dao.py
@@ -0,0 +1,144 @@
+from threading import Condition
+from dao import *
+from flask import session
+import boto3
+from pycognito import Cognito
+from boto3.dynamodb.conditions import Key, Attr, AttributeNotExists
+import sys
+
+db = boto3.resource('dynamodb')
+movie_table = db.Table('movie')
+review_table = db.Table('review')
+user_table = db.Table('user')
+
+user_pool_id = "ap-southeast-2_HXX7H0jCA"
+user_pool_client_id = "63k988au5jv73iubjgk5f8lqps"
+
+def post_review(username, movieid, review_text, vote):
+    success = False
+    reviewid = "{}_{}".format(username, movieid)
+    print("will post review: {} {} {}".format(reviewid, review_text, vote), file=sys.stderr)
+    try:
+    # if not review_table.get_item(Key={'reviewid':reviewid}):
+        review_table.put_item(
+            Item={
+                'reviewid': reviewid,
+                'user': username,
+                'movieid': movieid,
+                'movietitle': get_movie_by_id(movieid)['title'],
+                'review_text': review_text,
+                'vote': vote,
+            },
+        )
+    except:
+        print("couldn't post review", file=sys.stderr)
+    success = True
+    return success
+
+def register_user(email, username, password):
+    success = False
+    try:
+        new_user = Cognito(user_pool_id, user_pool_client_id)
+        new_user.set_base_attributes(email=email)
+        new_user.register(username, password)
+        user_table.put_item(
+            Item={
+                'username': username,
+                'favourites': []
+            }
+        )
+        success = True
+    except:
+        print("register user failed", file=sys.stderr)
+
+    return success
+
+def log_in_user(username, password):
+    success = False
+    login_user = Cognito(user_pool_id, user_pool_client_id,
+            username=username)
+    try:
+        login_user.authenticate(password=password)
+        success = True
+    except:
+        print("error logging in/authenticating user.", file=sys.stderr)
+    return success
+
+def update_favourite(addremove, movieid):
+    user_favourites = get_user(session['username'])['favourites']
+    if(addremove=="add"):
+        user_favourites.append(movieid)
+    elif(addremove=="remove"):
+        user_favourites.remove(movieid)
+    try:
+        user_table.update_item(
+            Key={
+                'username':session['username']
+            },
+            UpdateExpression='SET favourites = :val',
+            ExpressionAttributeValues={':val': user_favourites}
+        )
+    except:
+        print("updating favourite (add/remove) failed.", file=sys.stderr)
+    return  
+
+def get_favourites_details(favourites):
+    movie_details_list = []
+    try:
+        for movie in favourites:
+            movie_details_response = movie_table.get_item(Key={'movieid':movie})
+            movie_details = movie_details_response['Item']
+            movie_details_list.append(movie_details)
+    except:
+        print("Error getting favourites")
+    return movie_details_list
+
+def get_user(service, username):
+    try:
+        if service == "dynamodb":
+            user_response = user_table.get_item(Key={'username':username})
+            user = user_response['Item']
+        elif service == "cognito":
+            #TODO returns none, get_users() works
+            user_to_get = Cognito(user_pool_id, user_pool_client_id, username=username)
+            user = user_to_get.get_user_obj()
+    except:
+        user = None
+
+    return user
+def query_movie(term):
+    query_filter_list = []
+    movies = []
+    searchable_attributes = ['title', 'star', 'director', 'genre', 'country', 'released', 'classification']
+    try:
+        for att in searchable_attributes:
+            query_filter_list.append("Attr('{}').contains('{}')".format(att, term.lower()))
+        query_filter_expression = " | ".join(f for f in query_filter_list)
+        movie_raw_response = db.Table('movie_searchable').scan(
+            FilterExpression=eval(query_filter_expression)
+        )
+        movies = movie_raw_response['Items']
+    except:
+        print("movie query failed. returning empty result")
+    return movies
+
+def get_reviews(by, value):
+    reviews = []
+    try:
+        review_response = review_table.scan(
+            FilterExpression=Attr(by).eq(value)
+        )
+        reviews = review_response['Items']
+    except:
+        print("review query failed. returning empty result", file=sys.stderr)
+    return reviews
+
+def get_movie_by_id(movieid):
+    try:
+        movie_response = movie_table.get_item(Key={'movieid':movieid})
+        movie = movie_response['Item']
+    except:
+        movie = None
+
+
+    return movie
--- main.py
+++ main.py
@@ -1,24 +1,12 @@
-from threading import Condition
+from dao import *
 from flask import Flask, render_template, request, redirect, url_for, g, session
-import boto3
-from pycognito import Cognito
-from boto3.dynamodb.conditions import Key, Attr, AttributeNotExists
 import logging
 import math
-import requests
 
 logging.basicConfig(level=logging.DEBUG)
-db = boto3.resource('dynamodb')
-movie_table = db.Table('movie')
-review_table = db.Table('review')
-user_table = db.Table('user')
-user_pool_id = "ap-southeast-2_HXX7H0jCA"
-user_pool_client_id = "63k988au5jv73iubjgk5f8lqps"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'cc_assignment3_bestflix'
-
-db = boto3.resource('dynamodb')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -28,7 +16,8 @@ def index():
 @app.route('/movie/<string:movieid>', methods=['GET', 'POST'])
 def movie(movieid):
     show_post_error = False
-    session['user']['favourites'] = get_user(session['username'])['favourites']
+    if session['logged_in']:
+        session['user']['favourites'] = get_user("dynamodb", session['username'])['favourites']
     if request.method == 'POST':
         if request.form['form-type'] == "review":
             review_text = request.form['reviewarea']
@@ -76,7 +65,7 @@ def login():
         if(log_in_user(username, password)):
             app.logger.info("log in success")
             session['username'] = username
-            session['user'] = get_user(username)
+            session['user'] = get_user("dynamodb", username)
             session['logged_in'] = True
             app.logger.info("username: {}, logged_in: {}".format(session['username'], session['logged_in']))
             return redirect("/user/" + session['username'])
@@ -85,148 +74,20 @@ def login():
 
 @app.route('/user/<string:username>')
 def user(username):
-    user = get_user(username)
+    user = get_user("dynamodb", username)
+    cognito_user = get_user("cognito", username)
     user_favourites = get_favourites_details(user['favourites'])
     user_reviews = get_reviews("user", user['username'])
     app.logger.info("view user: " + username)
     app.logger.info(user_favourites)
 
-
-    return render_template('user.html', user=user, user_favourites=user_favourites, user_reviews=user_reviews)
+    return render_template('user.html', user=user, cognito_user=cognito_user, user_favourites=user_favourites, user_reviews=user_reviews)
 
 @app.route('/logout')
 def logout():
     session.pop('username', None)
     session.pop('logged_in', None)
     return redirect(url_for('login'))
-
-#region helper methods
-def post_review(username, movieid, review_text, vote):
-    success = False
-    reviewid = "{}_{}".format(username, movieid)
-    app.logger.info("will post review: {} {} {}".format(reviewid, review_text, vote))
-    # try:
-    # if not review_table.get_item(Key={'reviewid':reviewid}):
-    review_table.put_item(
-        Item={
-            'reviewid': reviewid,
-            'user': username,
-            'movieid': movieid,
-            'movietitle': get_movie_by_id(movieid)['title'],
-            'review_text': review_text,
-            'vote': vote,
-        },
-        ConditionExpression="attribute_not_exists(reviewid)"
-    )
-    success = True
-    # except:
-    #     app.logger.warning("couldn't post review")
-    return success
-
-def register_user(email, username, password):
-    success = False
-    try:
-        new_user = Cognito(user_pool_id, user_pool_client_id)
-        new_user.set_base_attributes(email=email)
-        new_user.register(username, password)
-        user_table.put_item(
-            Item={
-                'username': username,
-                'favourites': []
-            }
-        )
-        success = True
-    except:
-        app.logger.warning("register user failed")
-
-    return success
-
-def log_in_user(username, password):
-    success = False
-    login_user = Cognito(user_pool_id, user_pool_client_id,
-            username=username)
-    try:
-        login_user.authenticate(password=password)
-        success = True
-    except:
-        app.logger.warning("error logging in/authenticating user.")
-    return success
-
-def update_favourite(addremove, movieid):
-    user_favourites = get_user(session['username'])['favourites']
-    if(addremove=="add"):
-        user_favourites.append(movieid)
-    elif(addremove=="remove"):
-        user_favourites.remove(movieid)
-    try:
-        user_table.update_item(
-            Key={
-                'username':session['username']
-            },
-            UpdateExpression='SET favourites = :val',
-            ExpressionAttributeValues={':val': user_favourites}
-        )
-    except:
-        app.logger.warning("updating favourite (add/remove) failed.")
-    return  
-
-def get_favourites_details(favourites):
-    movie_details_list = []
-    try:
-        for movie in favourites:
-            movie_details_response = movie_table.get_item(Key={'movieid':movie})
-            movie_details = movie_details_response['Item']
-            movie_details_list.append(movie_details)
-    except:
-        print("Error getting favourites")
-    return movie_details_list
-
-
-def get_user(username):
-    try:
-        user_response = user_table.get_item(Key={'username':username})
-        user = user_response['Item']
-    except:
-        user = None
-
-    return user
-def query_movie(term):
-    query_filter_list = []
-    movies = []
-    searchable_attributes = ['title', 'star', 'director', 'genre', 'country', 'released']
-    try:
-        for att in searchable_attributes:
-            query_filter_list.append("Attr('{}').contains('{}')".format(att, term.lower()))
-        query_filter_expression = " | ".join(f for f in query_filter_list)
-        movie_raw_response = db.Table('movie_searchable').scan(
-            FilterExpression=eval(query_filter_expression)
-        )
-        movies = movie_raw_response['Items']
-    except:
-        app.logger.warning("movie query failed. returning empty result")
-    return movies
-
-def get_reviews(by, value):
-    reviews = []
-    try:
-        review_response = review_table.scan(
-            FilterExpression=Attr(by).eq(value)
-        )
-        reviews = review_response['Items']
-    except:
-        app.logger.warning("review query failed. returning empty result")
-    return reviews
-
-def get_movie_by_id(movieid):
-    try:
-        movie_response = movie_table.get_item(Key={'movieid':movieid})
-        movie = movie_response['Item']
-    except:
-        movie = None
-
-
-    return movie
-#endregion
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8080, debug=True)
