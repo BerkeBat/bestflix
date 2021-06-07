b05131d394832fc908746adae51349594674721a
Berke <s3782067@student.rmit.edu.au>
2021-06-07 19:57:28 +1000

    cleaning up for ebs deploy

--- app.py
+++ app.py
@@ -109,6 +109,6 @@ def logout():
     return redirect(url_for('login'))
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    app.run(host='127.0.0.1', port=8080, debug=True)
+#     app.run(host='127.0.0.1', port=8080, debug=True)
